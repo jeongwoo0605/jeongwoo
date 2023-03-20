8b55db66581c3e02b03a8cc934e215ea505d81fe
jeongwoo0605 <ghkdwjddn123@nate.com>
2023-03-20 17:13:49 +0900

    test commit1

---
+++ exam2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("%d", 10); // 변환문자열 %d(decimal)
+	printf("\n");
+	printf("%d", 10 + 20);
+	printf("%d", 20);
+	printf("%d %d", 10, 20);
+
+	return 0;
+}
